95d9650cc03b44b526abbf248cfa8d471e03dd86
manasprabhale <69726895+manasprabhale@users.noreply.github.com>
2026-01-10 23:06:20 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,10 @@
-// models/User.js
 const mongoose = require('mongoose');
 
-const policySchema = new mongoose.Schema({
-    policyNumber: { type: String, required: true },
-    dob: { type: Date, required: true },
-    premium: { type: Number, required: true },
-    mode: { type: String, required: true, enum: ['Monthly', 'Quarterly', 'Yearly'] }
-});
-
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    policies: [policySchema]  // Array of policies embedded in user
+    phone: { type: String }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
